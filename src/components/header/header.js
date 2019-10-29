20bbd58f6aa0cb7f4f7e6a7e3b9d043d55f57b79
Andrew Duong <duongsters@gmail.com>
2019-10-28 23:46:37 -0700

    added the header to be dynamically created when app.js runs

---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => { 
+    return(
+    <header className="gameboard-header">
+
+            <h1>Clicky-Game: NBA Edition</h1>
+
+    </header>
+)};
+
+export default Header;
