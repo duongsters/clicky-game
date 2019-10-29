ef6576b69ec32adfadff2145fb6b3e3c41110b00
Andrew Duong <duongsters@gmail.com>
2019-10-28 23:51:24 -0700

    added footer... that will be created when app.js runs

---
+++ src/components/footer/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+const Footer = () => {
+    return (
+        <footer className="Footer">
+
+                <a href=""><h3></h3></a>
+        </footer>
+    )
+};
+
+export default Footer;
